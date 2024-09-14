10b45871930b46d24b05d5cf6c1d5c5ee467b348
web3py-dev <89247604+web3py-dev@users.noreply.github.com>
2024-09-15 04:21:26 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var radius = 240; 
 var autoRotate = true; 
 var rotateSpeed = -60; 
@@ -32,7 +31,7 @@ function init(delayTime) {
   }
 }
 
-function applyTranform(obj) {
+function applyTransform(obj) {
   if (tY > 180) tY = 180;
   if (tY < 0) tY = 0;
   obj.style.transform = "rotateX(" + -tY + "deg) rotateY(" + tX + "deg)";
@@ -42,63 +41,95 @@ function playSpin(yes) {
   oimgs.style.animationPlayState = yes ? "running" : "paused";
 }
 
-var sX,
-  sY,
-  nX,
-  nY,
-  desX = 0,
-  desY = 0,
-  tX = 0,
-  tY = 10;
+var sX, sY, nX, nY, desX = 0, desY = 0, tX = 0, tY = 10;
 
 if (autoRotate) {
   var animationName = rotateSpeed > 0 ? "spin" : "spinRevert";
-  oimgs.style.animation = `${animationName} ${Math.abs(
-    rotateSpeed
-  )}s infinite linear`;
+  oimgs.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
 }
 
-document.onpointerdown = function(e) {
+function handlePointerDown(e) {
   clearInterval(odrag.timer);
   e = e || window.event;
-  var sX = e.clientX,
-    sY = e.clientY;
+  var sX = e.clientX, sY = e.clientY;
 
-  this.onpointermove = function(e) {
+  function handlePointerMove(e) {
     e = e || window.event;
-    var nX = e.clientX,
-      nY = e.clientY;
+    var nX = e.clientX, nY = e.clientY;
     desX = nX - sX;
     desY = nY - sY;
     tX += desX * 0.1;
     tY += desY * 0.1;
-    applyTranform(odrag);
+    applyTransform(odrag);
     sX = nX;
     sY = nY;
-  };
+  }
 
-  this.onpointerup = function(e) {
+  function handlePointerUp(e) {
     odrag.timer = setInterval(function() {
       desX *= 0.95;
       desY *= 0.95;
       tX += desX * 0.1;
       tY += desY * 0.1;
-      applyTranform(odrag);
+      applyTransform(odrag);
       playSpin(false);
       if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
         clearInterval(odrag.timer);
         playSpin(true);
       }
     }, 17);
-    this.onpointermove = this.onpointerup = null;
-  };
+    document.removeEventListener('pointermove', handlePointerMove);
+    document.removeEventListener('pointerup', handlePointerUp);
+  }
 
-  return false;
-};
+  document.addEventListener('pointermove', handlePointerMove);
+  document.addEventListener('pointerup', handlePointerUp);
+}
 
-document.onmousewheel = function(e) {
+function handleTouchStart(e) {
+  clearInterval(odrag.timer);
+  var touch = e.touches[0];
+  var sX = touch.clientX, sY = touch.clientY;
+
+  function handleTouchMove(e) {
+    var touch = e.touches[0];
+    var nX = touch.clientX, nY = touch.clientY;
+    desX = nX - sX;
+    desY = nY - sY;
+    tX += desX * 0.1;
+    tY += desY * 0.1;
+    applyTransform(odrag);
+    sX = nX;
+    sY = nY;
+  }
+
+  function handleTouchEnd() {
+    odrag.timer = setInterval(function() {
+      desX *= 0.95;
+      desY *= 0.95;
+      tX += desX * 0.1;
+      tY += desY * 0.1;
+      applyTransform(odrag);
+      playSpin(false);
+      if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
+        clearInterval(odrag.timer);
+        playSpin(true);
+      }
+    }, 17);
+    document.removeEventListener('touchmove', handleTouchMove);
+    document.removeEventListener('touchend', handleTouchEnd);
+  }
+
+  document.addEventListener('touchmove', handleTouchMove);
+  document.addEventListener('touchend', handleTouchEnd);
+}
+
+document.addEventListener('pointerdown', handlePointerDown);
+document.addEventListener('touchstart', handleTouchStart, { passive: false });
+
+document.onwheel = function(e) {
   e = e || window.event;
-  var d = e.wheelDelta / 20 || -e.detail;
+  var d = e.deltaY || -e.detail;
   radius += d;
   init(1);
 };
